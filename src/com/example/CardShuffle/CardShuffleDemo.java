56a3025e55f21a9aa637c87ff6265390821ead24
acdevine89 <annie.devine@detroitlabs.com>
2014-09-23 22:43:50 -0400

    Adding for loop to print out one suit as a test

--- src/com/example/CardShuffle/CardShuffleDemo.java
+++ src/com/example/CardShuffle/CardShuffleDemo.java
@@ -3,6 +3,38 @@ import java.util.ArrayList;
 
 public class CardShuffleDemo {
 
-ArrayList<Card> deckOfCards = new ArrayList<Card>();
+    public static void main(String[] args) {
+        ArrayList<Integer> deckOfCards = new ArrayList<Integer>();
+
+        for(int i=1; i<14; i++)
+        {
+            if(i==1)
+            {
+                System.out.println("Ace of hearts");
+            }
+
+            else if(i==11)
+            {
+                System.out.println("Jack of hearts");
+            }
+
+            else if(i==12)
+            {
+                System.out.println("Queen of hearts");
+            }
+
+            else if(i==13)
+            {
+                System.out.println("King of hearts");
+            }
+
+            else
+            {
+                System.out.println(i + " of hearts");
+            }
+        }
+
+    }
+
 
 }
