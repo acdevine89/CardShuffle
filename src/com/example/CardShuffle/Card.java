a002a567c3b441d37bbf890051a1378e926484a0
acdevine89 <annie.devine@detroitlabs.com>
2014-09-24 09:36:13 -0400

    Adding nested If statements to getCardSuit (not sure if this will work

--- src/com/example/CardShuffle/Card.java
+++ src/com/example/CardShuffle/Card.java
@@ -4,6 +4,11 @@ public class Card {
 
     private int cardSuit;
     private int cardNum;
+    private String hearts = "Hearts";
+    private String spades = "Spades";
+    private String clubs = "Clubs";
+    private String diamonds = "Diamonds";
+
 
     public Card(int cardSuit, int cardNum)
     {
@@ -18,6 +23,26 @@ public class Card {
 
     public int getCardSuit()
     {
+        if(cardSuit==1)
+        {
+            return Integer.parseInt(hearts);
+        }
+
+        if(cardSuit==2)
+        {
+            return Integer.parseInt(spades);
+        }
+
+        if(cardSuit==3)
+        {
+            return Integer.parseInt(clubs);
+        }
+
+        if(cardSuit==4)
+        {
+            return Integer.parseInt(diamonds);
+        }
+
         return cardSuit;
     }
 
--- src/com/example/CardShuffle/CardShuffleDemo.java
+++ src/com/example/CardShuffle/CardShuffleDemo.java
@@ -4,17 +4,31 @@ import java.util.ArrayList;
 public class CardShuffleDemo {
 
     public static void main(String[] args) {
+
+//public void createDeckOfCards()
+//    {
         ArrayList<Card> deckOfCards = new ArrayList<Card>();
 
-    for(int suit=1; suit<5; suit++)
-    {
-        for(int i=1; i<14; i++)
+        for (int suit=1; suit<5; suit++)
         {
-            Card newCard = new Card(suit, i);
-            System.out.println("Suit is " + suit + ", number is " + i);
-            deckOfCards.add(newCard);
+            for (int i=1; i<14; i++)
+            {
+                Card newCard = new Card(suit, i);
+                //System.out.println("Suit is " + suit + ", number is " + i);
+                deckOfCards.add(newCard);
+            }
         }
-    }
+
+        for (int i=0; i<53; i++)
+        {
+            //System.out.println(deckOfCards.get(1).getCardNum());
+            System.out.println(deckOfCards.get(1).getCardSuit());
+        }
+//
+//public void printDeckOfCards()
+//    {
+//
+//    }
 
 
         //            if(i==1)
@@ -39,6 +53,4 @@ public class CardShuffleDemo {
 //            }
 
     }
-
-
 }
