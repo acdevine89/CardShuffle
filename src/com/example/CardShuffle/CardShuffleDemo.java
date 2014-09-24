da21b116919f23a1c0fa1cec1115f85facdbfae6
acdevine89 <annie.devine@detroitlabs.com>
2014-09-24 16:01:40 -0400

    Deleting shuffle class because it's excessive

--- src/com/example/CardShuffle/CardShuffleDemo.java
+++ src/com/example/CardShuffle/CardShuffleDemo.java
@@ -7,34 +7,13 @@ public class CardShuffleDemo {
 
     public static void main(String[] args) {
 
-            List<Card> deckOfCards = new ArrayList<Card>();
-
-            for (int suit = 1; suit < 5; suit++) {
-                for (int i = 1; i < 14; i++) {
-                    Card newCard = new Card(suit, i);
-                    deckOfCards.add(newCard);
-                }
-            }
-
-    Collections.shuffle(deckOfCards);
-
-    for (int player=1; player<5; player++)
-    {
-        System.out.println("Player " + player + "'s hand:");
-
-        for (int card=0; card<5; card++)
-        {
-            System.out.println(deckOfCards.get(card).getCardNum() + " of " + deckOfCards.get(card).getCardSuit());
-            deckOfCards.remove(card);
-        }
-
-    }
 
     }
 
 //PRINTING ENTIRE DECK:
-//            for (int i = 0; i < 52; i++) {
-//                System.out.println(deckOfCards.get(i).getCardNum() + " of " + deckOfCards.get(i).getCardSuit());
-//            }
+//    for (int i = 0; i < 52; i++)
+//    {
+//    System.out.println(deckOfCards.get(i).getCardNum() + " of " + deckOfCards.get(i).getCardSuit());
+//    }
 
 }
--- src/com/example/CardShuffle/Deck.java
+++ src/com/example/CardShuffle/Deck.java
@@ -1,4 +1,39 @@
 package com.example.CardShuffle;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
+
+
+    public void createDeckOfCards()
+    {
+        List<Card> deckOfCards = new ArrayList<Card>();
+
+        for (int suit = 1; suit < 5; suit++) {
+            for (int i = 1; i < 14; i++) {
+                Card newCard = new Card(suit, i);
+                deckOfCards.add(newCard);
+            }
+        }
+    }
+
+
+    public void dealDeckOfCards()
+    {
+        Collections.shuffle(deckOfCards);
+
+        for (int player=1; player<5; player++)
+        {
+            System.out.println("Player " + player + "'s hand:");
+
+            for (int card=0; card<5; card++)
+            {
+                System.out.println(deckOfCards.get(card).getCardNum() + " of " + deckOfCards.get(card).getCardSuit());
+                deckOfCards.remove(card);
+            }
+
+        }
+    }
+
 }
