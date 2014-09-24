885a1690d0f632331e9c3ae7b2e7abba5ae1d851
acdevine89 <annie.devine@detroitlabs.com>
2014-09-24 14:45:14 -0400

    Adding for-loop that deals random hand of 5 cards

--- src/com/example/CardShuffle/CardShuffleDemo.java
+++ src/com/example/CardShuffle/CardShuffleDemo.java
@@ -1,54 +1,36 @@
 package com.example.CardShuffle;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class CardShuffleDemo {
 
     public static void main(String[] args) {
 
-//public void createDeckOfCards()
-//    {
-        ArrayList<Card> deckOfCards = new ArrayList<Card>();
+            List<Card> deckOfCards = new ArrayList<Card>();
 
-        for (int suit=1; suit<5; suit++)
-        {
-            for (int i=1; i<14; i++)
-            {
-                Card newCard = new Card(suit, i);
-                deckOfCards.add(newCard);
+            for (int suit = 1; suit < 5; suit++) {
+                for (int i = 1; i < 14; i++) {
+                    Card newCard = new Card(suit, i);
+                    deckOfCards.add(newCard);
+                }
             }
-        }
 
-        for (int i=0; i<52; i++)
+//            for (int i = 0; i < 52; i++) {
+//                System.out.println(deckOfCards.get(i).getCardNum() + " of " + deckOfCards.get(i).getCardSuit());
+//            }
+
+        Collections.shuffle(deckOfCards);
+
+    for (int player=0; player<4; player++)
+    {
+        for (int card=0; card<5; card++)
         {
-            System.out.println(deckOfCards.get(i).getCardNum() + " of " + deckOfCards.get(i).getCardSuit());
+            System.out.println(deckOfCards.get(card).getCardNum() + " of " + deckOfCards.get(card).getCardSuit());
         }
-//
-//public void printDeckOfCards()
-//    {
-//
-//    }
+    }
 
-
-        //            if(i==1)
-//            {
-//                newCard.setCardNum("Ace");
-//            }
-//
-//            else if(i==11)
-//            {
-//                newCard.setCardNum("Jack");
-//
-//            }
-//
-//            else if(i==12)
-//            {
-//                newCard.setCardNum("Queen");
-//            }
-//
-//            else if(i==13)
-//            {
-//                newCard.setCardNum("King");
-//            }
 
     }
+
 }
